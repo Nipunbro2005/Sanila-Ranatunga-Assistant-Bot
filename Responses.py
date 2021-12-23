207cc638c5753a31177c03e0e5001fa4daee3b71
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-12-23 17:45:36 +0530

    Add files via upload

--- Responses.py
+++ Responses.py
@@ -5,32 +5,45 @@ def sample_responses(input_text):
     user_message = str(input_text).lower()
 
     if user_message in ("1"):
-        return "I'm the Official Assistant Bot of Sanila Ranatunga😎🔥\n\n" \
-               "I can help you to solve your problems without connecting with Sanila. /help"
+        return "𝕊𝕒𝕟𝕚𝕝𝕒'𝕤 𝔸𝕤𝕤𝕚𝕤𝕥𝕒𝕟𝕥 𝔹𝕠𝕥\n𝕍𝕖𝕣𝕤𝕚𝕠𝕟: 𝟘.𝟝𝕧\n©𝟚𝟘𝟚𝟙"
+
 
     if user_message in ("2"):
-        return "Ok\n\nSend me the msg and I will send it to Sanila😏"
+        return "𝗔𝗯𝗼𝘂𝘁 𝗦𝗮𝗻𝗶𝗹𝗮\n\n" \
+               "❖ Name : Sanila Ranatunga😎\n\n" \
+               "❖ Age : 14 Years (2021) 🙃\n\n" \
+               "❖ Birthday : 2007.09.01\n\n" \
+               "❖ From : Sri Lanka🇱🇰\n\n" \
+               "❖ Skills : Programmer , Developer😏\n\n" \
+               "❖ Ambition : Be a software engineer😊\n\n" \
+               "❖ Programming Languages : Python🙃\n"
 
-    if user_message in ("3"):
-        return "Thanks for using my bots😋❤\n\n" \
-               "Send me the problems that you have faced in the bots."
+    if user_message in ("6"):
+        return "This is your time❤\n\nSend me your feedback🔥 "
 
-    if user_message in ("4"):
-        return "10 - Youtube Video Download Bot\n" \
-               "11 - Torrent Downloader Bot\n" \
-               "12 - Song Downloader Bot\n" \
-               "13 - Chat bot\n\n" \
+    if user_message in ("10"):
+        return "13 - Youtube Video Download Bot\n" \
+               "14 - Torrent Downloader Bot\n" \
+               "15 - Song Downloader Bot\n" \
+               "16 - Chat bot\n\n" \
                "Choose the bot and send me the number"
 
     if user_message in ("5"):
         return "Here's the github profile👇\n\nhttps://github.com/sanila2007"
 
-    if user_message in ("6"):
+    if user_message in ("3"):
         return "Changelog\n\n" \
+               "v0.5\n" \
+               " -Bug fixes and optimizations\n\n" \
+               "v0.4\n" \
+               " -Fixed problem in game\n" \
+               " -Changed the interface\n\n" \
+               "v0.3\n" \
+               " -Added time (US Time)\n\n" \
                "v0.2\n" \
                " -Changed the interface much attractive\n" \
                " -What's new changed to Changelog\n" \
-               " -Fixed problem in 12(Song downloader)\n" \
+               " -Fixed problem in 12 (Song downloader)\n" \
                " -Added time also\n" \
                " -Minor bugs fixes\n\n" \
                "v0.1\n" \
@@ -38,13 +51,25 @@ def sample_responses(input_text):
                " -Made much easier to use\n" \
                " -Improved Chat Facilities\n"
 
-    if user_message in ("7"):
-        return "Mathematics Game®\n\n𝕐𝕠𝕦𝕣 𝕘𝕒𝕞𝕖 𝕚𝕤 𝕔𝕦𝕣𝕣𝕖𝕟𝕥𝕝𝕪 𝕝𝕠𝕒𝕕𝕚𝕟𝕘..."
+    if user_message in ("8"):
+        return "Help Centre\n\n" \
+               "10 - How to use the bots\n" \
+               "11 - Not Responding error in bots\n" \
+               "12 - Always receiving error messages in bots\n\nChoose your problem and send me the number🔥"
+
+    if user_message in ("11"):
+        return "That will be a problem in server🥱\nWe will settle that problem soon😎\nThank you❤"
+
+    if user_message in ("12"):
+        return "Try again after 1 day and it will be automatically settled😉"
+
+    if user_message in ("4"):
+        return "𝕃𝕠𝕒𝕕𝕚𝕟𝕘..."
 
     if user_message in ("8"):
         return "Here's the Source Code👇👇\n\nhttps://github.com/sanila2007/Sanila-Ranatunga-Assistant-Bot"
 
-    if user_message in ("10"):
+    if user_message in ("13"):
         return "Youtube Video Downloader🎞\n\n" \
                "⭕️First use /start command in @youtubevideodownloader45_bot\n\n" \
                "⭕Second send the url of the video that you need to download.\n\n" \
@@ -53,7 +78,7 @@ def sample_responses(input_text):
                "you will receive a video.\n\n" \
                "Let's enjoy your video🤗\n\nHere's the bot👇👇\nhttps://t.me/youtubevideodownloader45_bot"
 
-    if user_message in ("11"):
+    if user_message in ("14"):
         return "Torrent Downloader🎥\n\n" \
                "⭕️First use /start command in @torrentdownloader88_bot\n\n" \
                "⭕️Second click the inline button. Either yts or else what you want.\n\n" \
@@ -61,7 +86,7 @@ def sample_responses(input_text):
                "⭕️Finally you will receive a torrent file link and click on it and download.\n\n" \
                "Enjoy🤗\n\nHere's the bot👇👇\nhttps://t.me/torrentdownloader88_bot"
 
-    if user_message in ("12"):
+    if user_message in ("15"):
         return "Song Download Bot🎵\n\n" \
                "⭕️First click the /start command in @songdownload597_bot\n\n" \
                "⭕️Second send me the song name. Don't only send the song name. You must send it like this Eg: /song " \
@@ -69,11 +94,15 @@ def sample_responses(input_text):
                "Enjoy your song🤗\n\nHere's the " \
                "bot👇\nhttps://t.me/songdownload597_bot "
 
-    if user_message in ("13"):
+    if user_message in ("16"):
         return "Chat Bot Advance📝\n\n" \
                "⭕️First use /start command in @useful_powerful_chat_bot\n\n" \
                "⭕️Second click the /help command and you will receive a msg that how to use that bot.\n\n" \
                "Enjoy your Chat Bot🤗\n\nHere's the bot👇\nhttps://t.me/useful_powerful_chat_bot"
+
+    if user_message in ("7"):
+        return "Send me the problems in Sanila's bots. Your bugs reports helps a " \
+               "lot to make those very powerful. Don't be shy, let's tell😉🤗😋"
 
     if user_message in ("time", "time"):
         now = datetime.now()
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from multiprocessing.process import name
+
 import telegram.ext
+
 from telegram.ext import updater
 
 import Constents as keys
@@ -7,18 +10,18 @@ import Responses as R
 print("Bot Started.....")
 
 def start_command(update, context):
-    update.message.reply_text("Hi dear,\nWelcome to Sanila Ranatunga's Official Assistant bot🤗🔥/help")
+    update.message.reply_text(f"Hi {name},\nWelcome to Sanila Ranatunga's Official Assistant bot🤗🔥/help")
 
 def help_command(update, context):
     update.message.reply_text("I can help you to connect with Sanila within seconds\n\n"
-                              "1 - Who am I\n"
-                              "2 - To connect with Sanila Ranatunga\n"
-                              "3 - Report bugs\n"
-                              "4 - To know how to use the Sanila's bots\n"
-                              "5 - Sanila's Github Profile\n"
-                              "6 - Changelog\n"
-                              "7 - Game\n"
-                              "8 - Source Code\n\n"
+                              "1 - About Bot\n"
+                              "2 - About Sanila\n"
+                              "3 - Changelog\n"
+                              "4 - Game\n"
+                              "5 - Github\n"
+                              "6 - Feedback\n"
+                              "7 - Report Bugs\n"
+                              "8 - Help Centre\n\n"
                               "🛑Just send me the number that you want to know\n"
                               "🛑Don't send texts because you won't get correct results")
 
@@ -30,8 +33,6 @@ def handle_message(update, context):
     update.message.reply_text(response)
 
 
-def error(update, context):
-    print(f"Update {update} caused error {context.error}")
 
 
 def main():
@@ -43,7 +44,7 @@ def main():
 
     dp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
 
-    dp.add_error_handler(error)
+
 
     updater.start_polling()
     updater.idle()
